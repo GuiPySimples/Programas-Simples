c1e001b136afb3640201320478a4dac1d839ffbe
GuiPySimples <niltovb@hotmail.com>
2017-02-20 19:24:10 -0300

    Programa que resolve equações de Segundo Grau
    Aqui está um programa que resolve equações de segundo grau

---
+++ eq2.py
@@ -0,0 +1,5 @@
+a = int(input("Qual o termo A da equação? "))
+b = int(input("Qual o termo B da equação? "))
+c = int(input("Qual o termo C da equação? "))
+
+print ("As raízes dessa equação são: {} e {} " '{0:,.1f}'.format (eq1 (a, b, c), eq2 (a, b, c) )
